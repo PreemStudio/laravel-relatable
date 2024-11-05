1ea4a2083776ea7306fc5074e0f4f86713bfbfce
Brian Faust <brian@faust.de.com>
2024-11-05 07:39:21 +0200

    first commit
    Signed-off-by: Brian Faust <brian@faust.de.com>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BaseCodeOy\Relatable;
+
+use BaseCodeOy\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    //
+}
